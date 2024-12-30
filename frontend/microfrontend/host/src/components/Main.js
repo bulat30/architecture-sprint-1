9af6d0a46ee57b09213ec62a1066dbe7ab1db750
Bulat Nabiulin <crowley.v1.0@gmail.com>
2024-12-30 21:23:54 +0300

    +

--- frontend/microfrontend/host/src/components/Main.js
+++ frontend/microfrontend/host/src/components/Main.js
@@ -3,14 +3,10 @@ import React, {lazy} from 'react';
 function Main() {
 
   const ProfileControl = lazy(() => import('profile/ProfileControl').catch((error) => {
-    console.log("ProfileControl error");
-    console.log(error);
     return { default: () => <div className='error'>Component is not available!</div> };
   }));
 
   const PlacesControl = lazy(() => import('places/PlacesControl').catch((error) => {
-    console.log("PlacesControl error");
-    console.log(error);
     return { default: () => <div className='error'>Component is not available!</div> };
   }));
 
